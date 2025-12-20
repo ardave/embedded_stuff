7250f45a533398fd428d8cfa0e91c40a1948a4a2
David Falkner <ardave@gmail.com>
2025-12-19 23:46:08 -0700

    polling switch gpio

--- C/blink_led/main/main.c
+++ C/blink_led/main/main.c
@@ -4,19 +4,20 @@
 #include "driver/gpio.h"
 
 #define BLINK_GPIO 8
-#define SWITCH_GPIO 999
+#define SWITCH_GPIO 4  // A0 pin on QT Py C3
 
 void app_main(void)
 {
     gpio_reset_pin(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 
+    gpio_reset_pin(SWITCH_GPIO);
     gpio_set_direction(SWITCH_GPIO, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(SWITCH_GPIO, GPIO_PULLUP_ONLY);
 
     while (1) {
-        gpio_set_level(BLINK_GPIO, 1);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(500 / portTICK_PERIOD_MS);
+        int switch_pressed = (gpio_get_level(SWITCH_GPIO) == 0);
+        gpio_set_level(BLINK_GPIO, switch_pressed);
+        vTaskDelay(10 / portTICK_PERIOD_MS);  // debounce delay
     }
 }
