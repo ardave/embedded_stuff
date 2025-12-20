a8b7f050c77a0fbdd4a949c6ca7f5ee49888ed83
David Falkner <ardave@gmail.com>
2025-12-19 23:40:56 -0700

    Timer blink is coded.  Going to mess with polling.

---
+++ C/blink_led/main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 8
+#define SWITCH_GPIO 999
+
+void app_main(void)
+{
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    gpio_set_direction(SWITCH_GPIO, GPIO_MODE_INPUT);
+
+    while (1) {
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(500 / portTICK_PERIOD_MS);
+    }
+}
