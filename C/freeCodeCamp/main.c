d9d355bc70db2404af6fef964393424440cbbf9b
David Falkner <ardave@gmail.com>
2026-01-14 14:23:50 -0700

    Initial commit of "frankensensor" STM32 project

--- C/freeCodeCamp/main.c
+++ C/freeCodeCamp/main.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    char name[50];
+    char major[50];
+    int age;
+    double gpa;
+
+};
 
 int main()
 {
-    int len = 20;
-    char name[len];
-    printf("Enter your name: ");
-    fgets(name, len, stdin);
-    printf("Your name is %s", name);
+    struct Student student1;
+    student1.age = 22;
+    student1.gpa = 3.2;
+    strcpy(student1.name, "Jim");
+    strcpy(student1.major, "Business");
+
+    printf("%f", student1.gpa);
     return 0;
 }
