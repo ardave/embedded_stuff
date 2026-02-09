58295d0841120634b24dafffc7fcb64fd854f62a
David Falkner <ardave@gmail.com>
2026-02-09 06:24:54 +0000

    separated gps task into new file

--- Rust/fitness-tracker/src/main.rs
+++ Rust/fitness-tracker/src/main.rs
@@ -1,4 +1,5 @@
-use esp_idf_svc::hal::task::thread::ThreadSpawnConfiguration;
+mod tasks;
+
 use log::info;
 use std::thread;
 use std::time::Duration;
@@ -11,25 +12,7 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    ThreadSpawnConfiguration {
-        name: Some(b"Poll GPS\0"),
-        stack_size: 4096,
-        priority: 10, // 1-24, higher = more priority│
-        inherit: false,
-        pin_to_core: None, // ESP32-S2 is single-core, doesn't matter
-        ..Default::default()
-    }
-    .set()
-    .expect("Failed to set thread config");
-
-    let _gps_thread = thread::Builder::new()
-        .name("Poll GPS".to_string())
-        .stack_size(4096)
-        .spawn(|| loop {
-            info!("Polling GPS...");
-            thread::sleep(Duration::from_secs(1));
-        })
-        .expect("Failed to span Poll GPS Task");
+    let _gps_thread = tasks::fake_gps::start();
 
     loop {
         info!("Hello, world!");
